ab42a01ef1b8f7d2db766bacf7276c44db5b2082
Hebe372 <60760295+Hebe372@users.noreply.github.com>
2020-02-14 22:21:23 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,35 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mes = document.getElementById("mes").value;
 
-//alert (mesDelAño);
-	
-	
-
-
+    switch (mes) {
+        case "Febrero":
+            alert("Este mes no tiene mas de 28 dias");
+            break;
+            case "Marzo":
+                    alert("Este mes tiene 31 dias");
+                    break;  
+                    case "Enero":
+                    alert("Este mes tiene 31 dias");
+                    break; 
+                    case "Mayo":
+                            alert("Este mes tiene 31 dias");
+                            break; 
+                            case "Julio":
+                            alert("Este mes tiene 31 dias");
+                            break;
+                            case "Agosto":
+                            alert("Este mes tiene 31 dias");
+                            break;
+                            case "Octubre":
+                            alert("Este mes tiene 31 dias");
+                            break;
+                            case "Diciembre":
+                            alert("Este mes tiene 31 dias");
+                            break;
+        default:
+            alert("Este mes tiene 30");
+            break;
+    }
 
 }//FIN DE LA FUNCIÓN
