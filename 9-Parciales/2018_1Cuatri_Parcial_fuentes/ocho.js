38103e92a7d1aa788231cfd189588fa961fbd889
Hebe372 <60760295+Hebe372@users.noreply.github.com>
2020-02-28 22:20:21 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,72 +1,82 @@
 function mostrar()
 {
-    var numero;
     var letra;
-    var acumuladorNumeros = -100;
-    var contadorPares;
-    var acumuladorPares;
-    var contadorImpares;
-    var acumuladorImpares;
-    var contadorCeros;
-    var acumuladorCeros;
-    var promedioPositivos;
-    var sumaNegativos;
+    var numero;
+    var seguir;
+    var contadorPares = 0;
+    var contadorImpares = 0;
+    var contadorCeros = 0;
+    var acumuladorPos = 0;
+    var contadorPos = 0;
+    var promedioPos = 0;
+    var acumuladorNeg = 0;
+    var numeroMax;
+    var numeroMin;
+    var letraMax;
+    var letraMin;
     var flag = 0;
-    var numeroDelAlto;
-    var numeroDelBajo;
-    var letraDelAlto;
-    var letraDelBajo;
-    var i >= -100;
-    
-    
-        // pido nota y la valido
-        
-        numero = parseInt(prompt("Ingrese numero(-100 a 100): "));
+
+
+    do {
+        letra = prompt("Ingrese una letra: ");
+        while (!((letra >= 'A' && letra <= 'Z') || (letra >= 'a' && letra <= 'z'))) {
+            letra = prompt("Eso no es una letra. Ingrese una letra: ");
+        }
+
+        numero = parseInt(prompt("Ingrese un numero (-100 y 100): "));
         while (numero < -100 || numero > 100 || isNaN(numero)) {
-            numero = parseInt(prompt("Numero invalido. Ingrese numero (-100 a 100): "));
-           i++ 
-        }
-        // pido sexo y lo valido
-        letra = prompt("Ingrese letra: ");
-        // while (letra != "f" && letra != "m") {
-           // sexo = prompt("Sexo invalido. Ingrese sexo: ");}
-
-        acumuladorNumeros = acumuladorNumeros + numero;
-
-        if (numero < numeroBajo || flag == 0) {
-            numeroBajo = numero;
-            letraDelBaj0 = letra;
-            flag = 1;
-        }
-        acumuladorNumeros = acumuladorNumeros + numero;
-
-        if (numero > numeroAlto || flag == 0) {
-            numeroAlto = numero;
-            letraDelAlto = letra;
-            flag = 1;
+            numero = parseInt(prompt("Numero invalido. Ingrese un numero (-100 y 100): "));
         }
 
-        if (acumuladorPares = acumuladorPares + numero) {
+        // ---------Pares-----------
+        if (numero % 2 == 0) {
+            // par
             contadorPares++;
         }
-
-        if (acumuladorImpares = acumuladorImpares + numero) {
+        else {
+            // impar
             contadorImpares++;
         }
-        if (acumuladorCeros = acumuladorCeros + numero) {
+
+        // ------------- Signo------------------
+
+        if (numero > 0) {
+            // positivos
+            acumuladorPos = acumuladorPos + numero;
+            contadorPos++;
+        }
+        else if (numero < 0) {
+            // negativos
+            acumuladorNeg = acumuladorNeg + numero;
+        }
+        else {
+            // cero
             contadorCeros++;
         }
+
+        // ------------- Maximos y minimos ---------------
+        if (flag == 0 || numero < numeroMin) {
+            numeroMin = numero;
+            letraMin = letra;
+        }
+        if (flag == 0 || numero > numeroMax) {
+            numeroMax = numero;
+            letraMax = letra;
+            flag = 1;
+        }
+
+        seguir = prompt("Quiere continuar?:");
+    } while (seguir == 's');
+
+    if (contadorPos != 0) {
+        promedioPos = acumuladorPos / contadorPos;
     }
 
-    promedioPositivos = acumuladorPositivos / ContadorPositivos;
-    sumaNegativos = acumularNegativos;
-
-    document.write("Cantidad de numeros pares: " + contadorPares + "</br>" 
-    + "Cantidad de numeros impares: " + contadorImpares + "</br>" 
-    + "Cantidad de ceros: " + contadorCeros + "</br>" 
-    + "El promedio de los numeros positivos es: " + promedioPositivos + "</br>" 
-    + "La suma de los numeros negativos es: " + sumaNegativos + "</br>" 
-    + "Numero minimo y su letra son: " + numeroDelBajo + letraDelBajo + "</br>" 
-    + "Numero maximo y su letra son: " + numeroDelAlto + letraDelAlto);
-
+    document.write("a) Cantidad de números pares: " + contadorPares + "</br>");
+    document.write("b) Cantidad de números impares: " + contadorImpares + "</br>");
+    document.write("c) Cantidad de ceros: " + contadorCeros + "</br>");
+    document.write("d) Promedio de todos los números positivos ingresados: " + promedioPos.toFixed(2) + "</br>");
+    document.write("e) La suma de todos los números negativos: " + acumuladorNeg + "</br>");
+    document.write("f) Número Máximo: " + numeroMax + " Letra: " + letraMax + "</br>");
+    document.write("f) Número Minimo: " + numeroMin + " Letra: " + letraMin + "</br>");
 }
